fae00008cff3d74d88363588600781e3f91d46fe
abhishekumar09 <2003abhishek.09@gmail.com>
2024-07-07 17:30:25 +0530

    learning about  structure of folders and files

---
+++ src/app/chai/layout.tsx
@@ -0,0 +1,12 @@
+export default function RootLayout({ 
+  children,                              // rootlayout accept  input parameter  as children
+}: Readonly<{                           // its typescript in which it defines the data types  like readonly datatype
+  children: React.ReactNode;              // children we set datatypes   reactnode type data accept liike example string type
+}>) {
+  return (
+    <>                  {/* use for wrapping the content */}
+    <h2>inner layout item</h2>
+    {children}
+    </>
+  );
+}
